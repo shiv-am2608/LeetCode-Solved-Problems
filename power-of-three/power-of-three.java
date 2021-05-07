4a21f924f61205287c1ed7ccaf11b51c480b5e87
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-05-07 18:12:21 +0530

    Time: 10 ms (99.90%), Space: 38.5 MB (69.88%) - LeetHub

--- power-of-three/power-of-three.java
+++ power-of-three/power-of-three.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n==0)
-            return false;
-        while(n%3==0)
+        while(n!=0 && n%3==0)
             n=n/3;
         return (n==1);
     }
