a10edefaee2bda7614063df62e11d1674907ce13
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-06-03 18:49:10 +0530

    Time: 7 ms (9.20%), Space: 42.5 MB (5.34%) - LeetHub

--- copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,8 +15,10 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
+        //checking if head is null
         if(head==null)
             return head;
+        //dealing with TC where single node exists
         if(head.next==null)
         {
             Node temp=new Node(head.val);
@@ -24,7 +26,11 @@ class Solution {
                 temp.random=temp;
             return temp;
         }
+        
+        //creating a treemap to maintain the order
         TreeMap<Integer,Node>hmap=new TreeMap<Integer,Node>();
+        
+        //dealt with first node explicitly
         Node copy=new Node(head.val);
         Node copy_ptr=copy;
         Node ptr=head.next;
@@ -33,6 +39,8 @@ class Solution {
         copy.random=head;
         head.next=copy;
         Node next=ptr.next;
+        
+        //establishing connections and storing reference of original LL in TreeMap
         while(ptr!=null)
         {
             Node temp=new Node(ptr.val);
@@ -47,18 +55,15 @@ class Solution {
                 break;
             next=next.next;
         }
-        hmap.put(count,null);
-        //Node check=head;
-        // while(check!=null)
-        // {
-        //     System.out.println(check.val+" "+" "+check.next.val);
-        //     check=check.next;
-        // }
+        hmap.put(count,null);//explicitly inserting null value in the end
+        
+        //connecting random pointers of copy LL 
         ptr=copy;
         while(ptr!=null)
         {
+            //dealing with cases where random might be pointing to null directly
             try{
-                ptr.random=ptr.random.random.next;
+                ptr.random=ptr.random.random.next;  
             }
             catch(Exception e)
             {
@@ -66,6 +71,8 @@ class Solution {
             }
             ptr=ptr.next;
         }
+        
+        //restoring the real connection of original linkedlist
         ptr=head;
         for(Map.Entry<Integer,Node>temp:hmap.entrySet())
         {
