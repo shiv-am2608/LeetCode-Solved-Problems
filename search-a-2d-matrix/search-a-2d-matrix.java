12ff14e60012f0c1c585dcef128c20e96d789c9a
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-06-08 13:46:03 +0530

    Time: 0 ms (100.00%), Space: 38.6 MB (24.73%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.java
+++ search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,16 +1,32 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int i=0,j=matrix[0].length-1;
-        while(i<matrix.length && matrix[i][j]<target)
-            i++;
-        if(i==matrix.length)
+    public int searchRow(int matrix[][],int target,int top,int bottom)
+    {
+        if(top>bottom)
+            return -1;
+        int row=(top+bottom)/2;
+        if(matrix[row][0]<=target && matrix[row][matrix[row].length-1]>=target)
+            return row;
+        else if(matrix[row][0]>target)
+            return searchRow(matrix,target,top,bottom-1);
+        return searchRow(matrix,target,top+1,bottom);     
+    }
+    public boolean searchElement(int arr[],int target,int left,int right)
+    {
+        if(left>right)
             return false;
-        while(j>=0 && matrix[i][j]>target)
-            j--;
-        if(j<0)
-            return false;
-        if(matrix[i][j]==target)
+        int mid=(left+right)/2;
+        if(arr[mid]==target)
             return true;
-        return false;
+        else if(arr[mid]<target)
+            return searchElement(arr,target,mid+1,right);
+        return searchElement(arr,target,left,mid-1);
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+       int rowNum=searchRow(matrix,target,0,matrix.length-1);
+        if(rowNum==-1)
+            return false;
+        int arr[]=matrix[rowNum];
+        return searchElement(arr,target,0,arr.length-1);
+        
     }
 }
