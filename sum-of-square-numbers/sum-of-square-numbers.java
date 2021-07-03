4d3e37bd17dd281f6b776101499d0800c1726d37
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-07-03 23:27:39 +0530

    Time: 2 ms (94.08%), Space: 35.6 MB (82.84%) - LeetHub

--- sum-of-square-numbers/sum-of-square-numbers.java
+++ sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,7 +1,10 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
+        //max possible range is 0 to Math.sqrt(c)
         int l=0;
         int r=(int)Math.sqrt(c);
+        
+        //applied two pointer approach
         while(l<=r)
         {
             int num=(l*l)+(r*r);
@@ -12,6 +15,6 @@ class Solution {
             else
                 r--;
         }
-        return false;
+        return false; 
     }
 }
