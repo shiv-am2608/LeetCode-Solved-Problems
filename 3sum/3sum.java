fa85b98cddadeaa8fb7e6ab513e73ca0699a5aae
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-07-03 22:38:21 +0530

    Time: 577 ms (14.30%), Space: 232.4 MB (5.00%) - LeetHub

---
+++ 3sum/3sum.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<Integer>>twoSum(int nums[],int start,int end,int target)
+    {
+        List<List<Integer>>ans=new ArrayList<List<Integer>>();
+        List<Integer>res=new ArrayList<Integer>();
+        while(start<end)
+        {
+            if(nums[start]+nums[end]==target)
+            {
+                res.add(nums[start]);
+                res.add(nums[end]);
+                ans.add(new ArrayList<>(res));
+                res.clear();
+                start++;
+                end--;
+            }
+            else if(nums[start]+nums[end]<target)
+                start++;
+            else
+                end--;
+        }
+        return ans;
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>>res=new ArrayList<List<Integer>>();
+        if(nums.length<3)
+            return res;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++)
+        {
+            List<List<Integer>>temp=twoSum(nums,i+1,nums.length-1,-nums[i]);
+            if(temp.size()>0)
+            {
+                for(List<Integer>x:temp)
+                {
+                    x.add(0,nums[i]);
+                    res.add(new ArrayList<>(x));
+                }
+            }
+        }
+        Set<List<Integer>> s = new LinkedHashSet<List<Integer>>(res); 
+        res.clear();
+        for(List<Integer>t:s)
+            res.add(t);
+        return res;
+    }
+}
