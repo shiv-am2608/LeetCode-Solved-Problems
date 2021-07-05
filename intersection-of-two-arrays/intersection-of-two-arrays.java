37615a6db2c213a07e6ec88667b58b495be0e58e
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-07-06 02:06:56 +0530

    Time: 2 ms (95.22%), Space: 39.1 MB (55.77%) - LeetHub

--- intersection-of-two-arrays/intersection-of-two-arrays.java
+++ intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        TreeSet<Integer>tset1=new TreeSet<Integer>();
-        TreeSet<Integer>tset2=new TreeSet<Integer>();
+        HashSet<Integer>hs1=new HashSet<>();
+        HashSet<Integer>hs2=new HashSet<>();
         for(int i=0;i<nums1.length;i++)
-            tset1.add(nums1[i]);
+            hs1.add(nums1[i]);
         for(int i=0;i<nums2.length;i++)
-            if(tset1.contains(nums2[i]))
-                tset2.add(nums2[i]);
-        int res[]=new int[tset2.size()];
+            if(hs1.contains(nums2[i]))
+                hs2.add(nums2[i]);
+        int res[]=new int[hs2.size()];
         int i=0;
-        for(int x:tset2)
+        for(int x:hs2)
             res[i++]=x;
         return res;
     }
