0d9c11bc77c16315e29ac28cede42d322d9cfab1
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-07-25 21:51:38 +0530

    Time: 1 ms (58.08%), Space: 39.5 MB (33.75%) - LeetHub

--- subsets/subsets.java
+++ subsets/subsets.java
@@ -1,27 +1,19 @@
 class Solution {
-    public void powerSet(List<Integer>res,int nums[],List<List<Integer>>ans,boolean visited[],int i)
+    public void powerSet(List<Integer>res,int nums[],List<List<Integer>>ans,int i)
     {
         ans.add(new ArrayList<>(res));
-        if(i==nums.length)
-            return;
         for(;i<nums.length;i++)
         {
-            if(!visited[i])
-            {
                 res.add(nums[i]);
-                visited[i]=true;
-                powerSet(res,nums,ans,visited,i+1);
-                visited[i]=false;
+                powerSet(res,nums,ans,i+1);
                 res.remove(res.size()-1);
-            }
         }
         
     }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>>ans=new ArrayList<List<Integer>>();
         List<Integer>res=new ArrayList<Integer>();
-        boolean visited[]=new boolean[nums.length];
-        powerSet(res,nums,ans,visited,0);
+        powerSet(res,nums,ans,0);
         return ans;
     }
 }
