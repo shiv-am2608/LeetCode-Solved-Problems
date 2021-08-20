0659ec9005a1a83e33f9d16e1eef6c5232047df2
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-08-20 21:18:10 +0530

    Time: 1 ms (100.00%), Space: 40.1 MB (29.94%) - LeetHub

---
+++ jump-game/jump-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(max<i)
+                return false;
+            max=Math.max(max,i+nums[i]);
+            if(max>=nums.length)
+                return true;
+        }
+        return true;
+    }
+}
