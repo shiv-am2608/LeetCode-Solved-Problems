5631b79ee88690e24ae7b5d77fa9196e4701657b
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-09-23 16:03:17 +0530

    Time: 3 ms (79.16%), Space: 39.3 MB (55.77%) - LeetHub

--- minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.java
+++ minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -1,26 +1,37 @@
 class Solution {
     public int minTaps(int n, int[] ranges) {
+        
+        //furthest distance from a point towards right
         int dist[]=new int[n+1];
+        boolean flag=false;
         for(int i=0;i<=n;i++)
         {
-            if(ranges[i]==0)
-                continue;
+            if(ranges[i]==0)//if range is 0 we ignore it
+                continue;      //*IMPORTANT* updates to i and may give ambiguity
             int left=Math.max(0,i-ranges[i]);
             int right=Math.min(n,i+ranges[i]);
+            if(right==n)
+                flag=true;
             dist[left]=Math.max(dist[left],right);
         }
-        int far=dist[0];
-        int tap=1;
+        if(!flag)
+            return -1;
+        int far=dist[0];//stores the farthest reachable distance with a tap
+        int tap=1;//stores the tap count
         if(far==n)
             return tap;
-        int next_tap=0;
+        int next_tap=0;//stores the index of next tap
+        
+        
         for(int i=0;i<n;i++)
         {
-            if(i>far)
+            if(i>far)//if we reach more than far it is unreacable
                 return -1;
-            next_tap=Math.max(next_tap,dist[i]);
-            if(next_tap==n)
+            next_tap=Math.max(next_tap,dist[i]);//updating next_tap index
+            if(next_tap==n)//if next tap is required at n it means we have watered all
                 return tap+1;
+            
+            //if we reach the max possible distance then we consider the next tap
             if(far==i)
             {
                 far=next_tap;
