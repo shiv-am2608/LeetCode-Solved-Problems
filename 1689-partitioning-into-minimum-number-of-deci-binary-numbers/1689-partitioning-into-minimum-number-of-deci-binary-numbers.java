812b258411a36bc35077c17276a8077d031f602d
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-09-27 19:37:38 +0530

    Delete 1689-partitioning-into-minimum-number-of-deci-binary-numbers directory

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int minPartitions(String n) {
-        int max=0;
-        for(int i=0;i<n.length();i++)
-            if(n.charAt(i)-'0'>max)
-                max=n.charAt(i)-'0';
-        return max;
-    }
-}
