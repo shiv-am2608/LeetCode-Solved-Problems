554bbc5a88cc2dd99e3ddbf38801f179edf789d4
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-10-10 12:44:21 +0530

    Time: 2 ms (81.65%), Space: 39.4 MB (54.38%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,34 +1,16 @@
 class Solution {
-    public int lomuto(int nums[],int left,int right)
-    {
-        int i=left-1;
-        for(int j=left;j<right;j++)
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer>pq=new PriorityQueue<Integer>();
+        for(int i=0;i<k;i++)
+            pq.offer(nums[i]);
+        for(int i=k;i<nums.length;i++)
         {
-            if(nums[right]<=nums[j])
+            if(pq.peek()<nums[i])
             {
-                i++;
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
+                pq.poll();
+                pq.offer(nums[i]);
             }
         }
-        int temp=nums[i+1];
-        nums[i+1]=nums[right];
-        nums[right]=temp;
-        return i+1;
-    }
-    public int findKthLargest(int[] nums, int k) {
-        int left=0,right=nums.length-1;
-        while(left<=right)
-        {
-            int pivot=lomuto(nums,left,right);
-            if(pivot==k-1)
-                return nums[pivot];
-            if(pivot>k-1)
-                right=pivot-1;
-            else
-                left=pivot+1;
-        }
-        return nums[0];
+        return pq.peek();
     }
 }
