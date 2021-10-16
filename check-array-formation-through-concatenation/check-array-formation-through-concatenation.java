5e5f795b910083bffb846f5b484c493b0913dea7
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-10-16 14:51:34 +0530

    Time: 3 ms (20.22%), Space: 39.5 MB (14.62%) - LeetHub

---
+++ check-array-formation-through-concatenation/check-array-formation-through-concatenation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for(int i=0; i<arr.length; i++){
+            hm.put(arr[i], i);
+        }
+        
+        for(int[] i: pieces){
+            if(!hm.containsKey(i[0]))
+                return false;
+            
+            for(int j=1; j<i.length; j++){
+                if(!hm.containsKey(i[j]))
+                    return false;
+                if(hm.get(i[j]) - hm.get(i[j-1]) != 1)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
