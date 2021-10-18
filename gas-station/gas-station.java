f8bff0149e370b412fce2751f171662cda37983c
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-10-18 21:04:51 +0530

    Time: 2 ms (43.15%), Space: 63.9 MB (5.01%) - LeetHub

--- gas-station/gas-station.java
+++ gas-station/gas-station.java
@@ -1,34 +1,22 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        // int max=Integer.MIN_VALUE;
-        // int index=-1;
-        // for(int i=0;i<gas.length;i++)
-        //     if(max<gas[i]-cost[i])
-        //     {
-        //         max=gas[i]-cost[i];
-        //         index=i;
-        //     }
-        // if(max<0)
-        //     return -1;
-        for(int index=0;index<gas.length;index++)
+        int tank=0; //current amount in tank
+        int shortage=0; //shortage in tank
+        int start=0;    //staring point
+        for(int i=0;i<gas.length;i++)
         {
-            if(gas[index]-cost[index]<0)
-                continue;
-            int j=(index+1)%gas.length;
-            int fuel=gas[index]-cost[index];
-            while(j!=index)
+            tank+=gas[i]-cost[i];
+            
+            //if their is a shortage,ww will assume next point as start
+            if(tank<0)
             {
-                fuel+=gas[j]-cost[j];
-                if(fuel<0)
-                    break;
-                j=(j+1)%gas.length;
+                shortage+=tank;
+                tank=0;
+                start=i+1;
             }
-            if(j==index)
-                return index;
         }
-        return -1;
-            // if(j!=index)
-            //     return -1;
-            // return index;
+        
+        //checking if final shortage can be covered with the remaining fuel in tank
+        return shortage+tank>=0?start:-1;
     }
 }
