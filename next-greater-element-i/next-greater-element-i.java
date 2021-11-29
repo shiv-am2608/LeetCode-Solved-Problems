b37e7a73e2697100a358729789a0e076b83a34cb
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-11-29 23:37:52 +0530

    Time: 4 ms (72.18%), Space: 39.1 MB (89.13%) - LeetHub

--- next-greater-element-i/next-greater-element-i.java
+++ next-greater-element-i/next-greater-element-i.java
@@ -1,28 +1,25 @@
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer>hmap=new HashMap<Integer,Integer>();
-        for(int i=0;i<nums2.length;i++)
-            hmap.put(nums2[i],i);
-        int next[]=new int[nums2.length];
+    public static HashMap<Integer,Integer> nextLargerElement(int[] arr, int n)
+    { 
         Stack<Integer>st=new Stack<Integer>();
-        next[next.length-1]=-1;
-        st.push(nums2[next.length-1]);
-        for(int i=nums2.length-2;i>=0;i--)
+        HashMap<Integer,Integer>hmap=new HashMap<Integer,Integer>();
+        for(int i=n-1;i>=0;i--)
         {
-            while(!st.isEmpty() && st.peek()<=nums2[i])
+            while(!st.isEmpty() && st.peek()<arr[i])
                 st.pop();
             if(st.isEmpty())
-                next[i]=-1;
+                hmap.put(arr[i],-1);
             else
-                next[i]=st.peek();
-            st.push(nums2[i]);
+                hmap.put(arr[i],st.peek());
+            st.push(arr[i]);
         }
-        
-        int ans[]=new int[nums1.length];
+        return hmap;
+    } 
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer>hmap=nextLargerElement(nums2,nums2.length);
+        int res[]=new int[nums1.length];
         for(int i=0;i<nums1.length;i++)
-        {
-            ans[i]=next[hmap.get(nums1[i])];
-        }
-        return ans;
+            res[i]=hmap.get(nums1[i]);
+        return res;
     }
 }
