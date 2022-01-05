293fc0cece79589ebeaa51d009ed38d1ef9eb4ee
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-01-05 18:26:53 +0530

    Time: 0 ms (100.00%), Space: 37.1 MB (91.74%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean dfs(String s1,String s2,String s3,int i,int j,int k,boolean invalid[][])
+    {
+        if(invalid[i][j])
+            return false;
+        if(k==s3.length())
+            return true;
+        boolean valid=false;
+        if(i<s1.length() && s1.charAt(i)==s3.charAt(k))
+            valid=dfs(s1,s2,s3,i+1,j,k+1,invalid);
+        if(!valid && j<s2.length() && s2.charAt(j)==s3.charAt(k))
+            valid=dfs(s1,s2,s3,i,j+1,k+1,invalid);
+        if(!valid)
+            invalid[i][j]=true;
+        return valid;
+    }
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s1.length()+s2.length()!=s3.length())
+            return false;
+        return dfs(s1,s2,s3,0,0,0,new boolean[s1.length()+1][s2.length()+1]);
+    }
+}
