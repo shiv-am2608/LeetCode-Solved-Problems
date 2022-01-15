03eda18824cc757cfad7253da01e3053fe62571c
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-01-15 11:26:37 +0530

    Time: 5 ms (60.30%), Space: 42.3 MB (39.19%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int freq[]=new int[26];
+        for(int i=0;i<magazine.length();i++)
+            freq[magazine.charAt(i)-'a']++;
+        for(int i=0;i<ransomNote.length();i++)
+            freq[ransomNote.charAt(i)-'a']--;
+        for(int i=0;i<26;i++)
+            if(freq[i]<0)
+                return false;
+        return true;
+    }
+}
