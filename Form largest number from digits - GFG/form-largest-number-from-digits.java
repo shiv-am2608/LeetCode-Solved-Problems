a4061373478399df882eeff6b9354e5f9f819cf8
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-01-18 13:33:58 +0530

    Added solution - LeetHub

---
+++ Form largest number from digits - GFG/form-largest-number-from-digits.java
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution ob = new Solution();
+            System.out.println(ob.MaxNumber(a, n));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    
+    public String MaxNumber(long arr[], long n)
+    {
+        //code here.
+        StringBuilder s = new StringBuilder();
+        Arrays.sort(arr);
+        for(int i = 0; i < n; i++)
+           s.append(arr[i]);
+       return s.reverse().toString();
+    }
+}
