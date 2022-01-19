5c2fd49d74a5a0dfdc87e8649b35ba0ae09a04cd
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-01-19 12:43:30 +0530

    Added solution - LeetHub

---
+++ Alien Dictionary - GFG/alien-dictionary.java
@@ -0,0 +1,102 @@
+// { Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = Integer.parseInt(sc.next());
+		while(t-- > 0)
+		{
+		    int n = Integer.parseInt(sc.next());
+		    int k = Integer.parseInt(sc.next());
+		    
+		    String[] words = new String[n];
+		    
+		    for(int i=0;i<n;i++)
+		    {
+		        words[i] = sc.next();
+		    }
+		    
+		    Solution ob = new Solution();
+		  //  System.out.println(T.findOrder(words,k));
+		    String order = ob.findOrder(words,n,k);
+		    if(order.length() == 0){
+		        System.out.println(0);
+		        continue;
+		    }
+		    String temp[] = new String[n];
+		    for(int i=0;i<n;i++)
+		        temp[i] = words[i];
+		    
+		    Arrays.sort(temp, new Comparator<String>(){
+		    
+		      @Override
+                public int compare(String a, String b) {
+                    int index1 = 0;
+                    int index2 = 0;
+                    for(int i = 0; i < Math.min(a.length(), b.length()) 
+                                        && index1 == index2; i++) {
+                        index1 = order.indexOf(a.charAt(i));
+                        index2 = order.indexOf(b.charAt(i));
+                    }
+                
+                    if(index1 == index2 && a.length() != b.length()) 
+                    {
+                        if(a.length() < b.length())
+                            return 1;
+                        else
+                            return 0;
+                    }
+                
+                    if(index1 < index2)
+                        return 1;
+                    else
+                        return 0;
+                        
+                }
+		    });
+		    
+		    int flag = 1;
+		    for(int i=0;i<n;i++)
+		    {
+		        if(!words[i].equals(temp[i]))
+	            {
+	                flag = 0;
+	                break;
+	            }
+		    }
+		    
+		    System.out.println(flag);
+		}
+	}
+	
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String findOrder(String [] dict, int N, int K)
+    {
+        // Write your code here
+        Set<Character> set = new HashSet<>();
+       for (String s : dict) {
+           for (char c: s.toCharArray()) {
+               set.add(c);
+           }
+       }
+       StringBuilder b = new StringBuilder();
+       for (char c : set) {
+           b.append(c);
+       }
+       return b.toString();
+        
+    }
+}
