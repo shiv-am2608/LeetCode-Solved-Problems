7cf13de274eee4dcdfd20f3ad2a3d97c11a58ec0
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-01-23 01:20:08 +0530

    Added solution - LeetHub

---
+++ IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.java
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main {
+    public static void main(String args[]) {
+        // taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // taking total count of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // taking total number of elements
+            int n = sc.nextInt();
+
+            // taking size of subArrays
+            int k = sc.nextInt();
+
+            // Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+
+            // adding all the elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            // Calling the method max_of_subarrays of class solve
+            // and storing the result in an ArrayList
+            ArrayList<Integer> res = new Solution().max_of_subarrays(arr, n, k);
+
+            // printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution {
+    static ArrayList<Integer> max_of_subarrays(int arr[], int n, int k) {
+        // Your code here
+        ArrayList<Integer>ans=new ArrayList<Integer>();
+        Deque<Integer> dq = new LinkedList<Integer>();
+        for (int i=0;i<k;i++)
+        {
+            while (!dq.isEmpty() && arr[i]>=arr[dq.peekLast()])
+                dq.pollLast();
+            dq.offerLast(i);
+        }
+        ans.add(arr[dq.peek()]);
+        for (int i=k;i<n;i++)
+        {
+            while (!dq.isEmpty() && dq.peek()<=i-k)
+                dq.removeFirst();
+            while(!dq.isEmpty() && arr[i]>=arr[dq.peekLast()])
+                dq.removeLast();
+            dq.addLast(i);
+            ans.add(arr[dq.peek()]);
+        }
+        return ans;
+    }
+}
