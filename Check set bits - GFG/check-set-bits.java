67554459cc3fb19f0bc1702c7f5cc07bf2f2d68e
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-01-23 19:34:32 +0530

    Added solution - LeetHub

---
+++ Check set bits - GFG/check-set-bits.java
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.isBitSet(N));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int isBitSet(int N){
+        //code here
+        if(N==0)
+            return 0;
+        while(N>0)
+        {
+            if((N&1)==0)
+                return 0;
+            N=N>>1;
+        }
+        return 1;
+    }
+}
