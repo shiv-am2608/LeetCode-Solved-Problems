276a66354bd18c656448c23a510f86b7fa44896a
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-01-30 13:42:05 +0530

    Added solution - LeetHub

---
+++ Quick Sort - GFG/quick-sort.java
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+import java.util.*;
+class Sorting
+{
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    
+    // Driver program
+    public static void main(String args[])
+    {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+			
+			new Solution().quickSort(arr,0,n-1);
+			printArray(arr);
+		    T--;
+		}
+} }// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+    static void swap(int arr[],int i,int j)
+    {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    static void quickSort(int arr[], int low, int high)
+    {
+        // code here
+        if(low>=high)
+            return;
+        int pi=partition(arr,low,high);
+        quickSort(arr,low,pi-1);
+        quickSort(arr,pi+1,high);
+    }
+    static int partition(int arr[], int low, int high)
+    {
+        // your code here
+        int num=arr[high];
+        int k=low-1;
+        for(int i=low;i<high;i++)
+        {
+            if(arr[i]<arr[high])
+            {
+                k++;
+                swap(arr,i,k);
+            }
+        }
+        swap(arr,k+1,high);
+        return k+1;
+    } 
+}
