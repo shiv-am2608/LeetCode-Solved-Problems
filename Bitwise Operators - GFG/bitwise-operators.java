621946b3467b8e8d122cc6e631546dcfccc9c197
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-02-07 23:00:44 +0530

    Added solution - LeetHub

---
+++ Bitwise Operators - GFG/bitwise-operators.java
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) {
+
+        // taking input using scanner class
+        Scanner sc = new Scanner(System.in);
+
+        int testcases = sc.nextInt();
+
+        while (testcases-- > 0) {
+
+            // taking a,b,c
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            // creating an object of class Geeks
+            Geeks g = new Geeks();
+
+            // calling the method bitWiseOp()
+            // of class Geeks and passing
+            // a,b,c as arguments
+            g.bitWiseOperation(a, b, c);
+        }
+    }
+}// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Geeks {
+    // Function to perform bitwise manipulations
+    // a, b and c are input integers
+    static void bitWiseOperation(int a, int b, int c) {
+
+        // code here
+        int d = a^a;
+        int e = c^b;
+        int f = a&b;
+        int g = c | (a^a);
+        int h= ~e ;
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
