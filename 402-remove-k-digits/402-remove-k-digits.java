9cde8978160a56ba4298efcd5f050231f05aeecf
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-02-19 00:00:45 +0530

    Time: 11 ms (69.07%), Space: 42.4 MB (55.89%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        int len = num.length();
+        if(k==len)        
+            return "0";
+            
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<len;i++)
+        {
+            //whenever meet a digit which is less than the previous digit, discard the previous one
+            while(k>0 && !stack.isEmpty() && stack.peek()>num.charAt(i)){
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(i));
+        }
+        
+        // corner case like "1111"
+        while(k>0){
+            stack.pop();
+            k--;            
+        }
+        
+        //construct the number from the stack
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty())
+            sb.append(stack.pop());
+        sb.reverse();
+        
+        //remove all the 0 at the head
+        while(sb.length()>1 && sb.charAt(0)=='0')
+            sb.deleteCharAt(0);
+        return sb.toString();
+    }
+}
