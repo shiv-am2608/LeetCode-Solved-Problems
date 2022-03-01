c769fa3ce67f1266a9597a11dbb33dff1b7d6309
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-03-01 22:45:05 +0530

    Added solution - LeetHub

---
+++ 15. Smallest Positive missing number - GFG/15.-smallest-positive-missing-number.java
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+import java.util.*;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size)
+    {
+        // Your code here
+        for(int i=0;i<size;i++)
+        {
+            int pos=arr[i]-1;
+            while((arr[i]>=1 && arr[i]<=size) && arr[i]!=arr[pos] )
+            {
+                int temp=arr[i];
+                arr[i]=arr[pos];
+                arr[pos]=temp;
+                pos=arr[i]-1;
+            }
+        }
+        for(int i=0;i<size;i++)
+            if(arr[i]!=i+1)
+                return i+1;
+        return size+1;
+        
+    }
+}
+
+
+// { Driver Code Starts.
+
+class Main
+{   
+    public static void main (String[] args) 
+    {
+
+		Scanner sc=new Scanner(System.in);
+		
+        //taking testcases
+		int t=sc.nextInt();
+		while(t-->0){
+		    
+		    //input number n
+			int n=sc.nextInt();
+			int[] arr=new int[n];
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i]=sc.nextInt();
+				
+			Solution obj = new Solution();
+			
+			//calling missingNumber()
+			int missing = obj.missingNumber(arr,n);
+			System.out.println(missing);
+		}
+    }
+}
+
+  // } Driver Code Ends
