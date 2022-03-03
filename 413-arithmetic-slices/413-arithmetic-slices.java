c81a079e9d3d27693eccb0d4a3519b2c36a4b015
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-03-04 00:01:53 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (6.96%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int slices = 0;
+	    for (int i = 2, prev = 0; i < nums.length; i++)
+		    slices += (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) ? ++prev : (prev = 0);
+	    return slices;
+    }
+}
