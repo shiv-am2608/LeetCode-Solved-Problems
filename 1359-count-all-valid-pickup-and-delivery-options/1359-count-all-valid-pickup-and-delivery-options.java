f1412a019026ae4cecd5a5ab39562da615556380
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-03-06 19:13:29 +0530

    Time: 1 ms (54.21%), Space: 41 MB (33.41%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countOrders(int n) {
+        long res = 1, modulo = 1000000007;
+        for(int i=1; i<=n; i++){
+            res = res * i * (2*i - 1) % modulo;
+        }
+        return (int)res;
+    }
+}
