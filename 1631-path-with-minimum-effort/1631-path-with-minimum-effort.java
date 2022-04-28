8ba419ebce92184bfb08fe464c937cabdcc07b25
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-04-28 12:42:29 +0530

    Time: 117 ms (30.19%), Space: 60.2 MB (39.25%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -7,14 +7,15 @@ class Solution {
         if(n==1 && m==1)
             return 0;
         
-        int efforts[][]=new int[n][m];
+        int efforts[][]=new int[n][m];//stores the minimum effots required to travel upto a given cell
         for(int row[]:efforts)
             Arrays.fill(row,Integer.MAX_VALUE);
         
-        PriorityQueue<int[]>pq=new PriorityQueue<int[]>((t1,t2)->(t1[2]-t2[2]));
+        PriorityQueue<int[]>pq=new PriorityQueue<int[]>((t1,t2)->(t1[2]-t2[2]));//returns cell with minimum efforts
         
         pq.offer(new int[]{0,0,0});
-        int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};
+        int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};//direction to travel
+        
         while(!pq.isEmpty())
         {
             int curr[]=pq.poll();
@@ -27,7 +28,8 @@ class Solution {
                 int ncol=curr_col+x[1];
                 if(nrow<0 || nrow>=n || ncol<0 || ncol>=m)
                     continue;
-                int wt = Math.max(curr_wt,Math.abs(heights[nrow][ncol]-heights[curr_row][curr_col]));
+                int wt = Math.max(curr_wt,Math.abs(heights[nrow][ncol]-heights[curr_row][curr_col]));//getting max absolute value
+                //updating the minimum effort
                 if(wt<efforts[nrow][ncol])
                 {
                     efforts[nrow][ncol]=wt;
