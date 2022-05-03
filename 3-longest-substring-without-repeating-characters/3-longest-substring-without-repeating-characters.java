02dc27a1f709ae039facdad0a34c922d0103fe02
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-05-03 19:25:26 +0530

    Time: 12 ms (48.79%), Space: 44.6 MB (54.22%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -3,15 +3,10 @@ class Solution {
         //hashmap stores character and index of last occurance of character
         HashMap<Character,Integer>hmap=new HashMap<Character,Integer>();
         int length=0,res=0,j=-1;
-        
-        //iterating over the string
         for(int i=0;i<s.length();i++)
         {
-            //if we find duplicate we begin again from next index of last occurance of                  repeating character
             if(hmap.containsKey(s.charAt(i)))
-            {
-                j=Math.max(j,hmap.get(s.charAt(i)));//updates the i value for loop to go back in                      index
-            }
+                j=Math.max(j,hmap.get(s.charAt(i)));
             hmap.put(s.charAt(i),i);
             length=i-j;;
             res=Math.max(res,length);//storing the max result
