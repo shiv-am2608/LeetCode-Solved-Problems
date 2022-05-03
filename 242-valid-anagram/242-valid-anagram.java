03afb59b326fbee577d66b65c50d080b81f91926
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2022-05-03 18:01:19 +0530

    Time: 5 ms (65.15%), Space: 43.4 MB (65.63%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -4,9 +4,10 @@ class Solution {
             return false;
         int freq[]=new int[26];
         for(int i=0;i<s.length();i++)
+        {
             freq[s.charAt(i)-'a']++;
-        for(int i=0;i<t.length();i++)
             freq[t.charAt(i)-'a']--;
+        }
         for(int i=0;i<26;i++)
             if(freq[i]!=0)
                 return false;
